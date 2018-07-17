f897a204f9f20ecdf946f443faf3bc7909a7d320
Tibor Kiss <tibor.kiss@t-systems.com>
2018-07-17 14:11:45 +0200

    WarriorWars

---
+++ C#/WarriorWars/WarriorWars/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using WarriorWars.Enum;
+
+namespace WarriorWars
+{
+    class Program
+    {
+        static Random rnd = new Random();
+
+
+
+        static void Main()
+        {
+            Warrior goodGuy = new Warrior("Tibi", Faction.GoodGuy);
+            Warrior badGuy = new Warrior("Ricsi", Faction.BadGuy);
+
+            while (goodGuy.IsAlive && badGuy.IsAlive)
+            {
+                if (rnd.Next(0,10) < 5)
+                {
+                    goodGuy.Attack(badGuy);
+                }
+                else
+                {
+                    badGuy.Attack(goodGuy);
+                }
+            }
+            
+        }
+    }
+}
---
+++ C#/WarriorWars/WarriorWars/Warrior.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading;
+using WarriorWars.Enum;
+using WarriorWars.Equipment;
+
+namespace WarriorWars
+{
+    class Warrior
+    {
+        private const int GOOD_GUY_STARTING_HEALTH = 100;
+        private const int BAD_GUY_STARTING_HEALTH = 100;
+
+        private readonly Faction FACTION;
+
+        private int health;
+        private string name;
+        private bool isAlive;
+
+        public bool IsAlive
+        {
+            get
+            {
+                return isAlive;
+            }
+        }
+
+        private Weapon weapon;
+        private Armor armor;
+
+        
+        public Warrior(string name, Faction faction)
+        {
+            this.name = name;
+            this.FACTION = faction;
+
+            isAlive = true;
+
+            switch (faction)
+            {
+                case Faction.GoodGuy:
+                    weapon = new Weapon(faction);
+                    armor = new Armor(faction);
+                    this.health = GOOD_GUY_STARTING_HEALTH;
+
+                    break;
+                case Faction.BadGuy:
+                    weapon = new Weapon(faction);
+                    armor = new Armor(faction);
+                    this.health = BAD_GUY_STARTING_HEALTH;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        public void Attack(Warrior enemy)
+        {
+            int damage = weapon.Damage / enemy.armor.ArmorPoints;
+
+            enemy.health -= damage;
+
+            AttackResult(enemy, damage);
+
+            Thread.Sleep(20);
+        }
+
+        private void AttackResult(Warrior enemy, int damage)
+        {
+            if (enemy.health <= 0)
+            {
+                enemy.isAlive = false;
+                Tools.ColorfulWriteLine($"{enemy.name} is dead!", ConsoleColor.Red);
+                Tools.ColorfulWriteLine($"{name} is Victorious!", ConsoleColor.Green);
+                System.Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine($"{name} attacked {enemy.name}. {damage} was inflected to {enemy.name}, remaining health of {enemy.name} is {enemy.health}.");
+            }
+        }
+    }
+}
